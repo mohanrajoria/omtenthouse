f43c35c7fb128df98c5eeacbec69350bffee23cf
Mohan Rajoria <rajoria7@gmail.com>
2023-02-07 18:48:47 +0530

    image uploader

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect, useRef } from 'react'
 import Bridge from '../components/Icons/Bridge'
-import Logo from '../components/Icons/Logo'
 import Modal from '../components/Modal'
 import cloudinary from '../utils/cloudinary'
 import getBase64ImageUrl from '../utils/generateBlurPlaceholder'
@@ -57,22 +56,18 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               </span>
               <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
             </div>
-            <Logo />
-            <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              2022 Event Photos
+            <h1 className="mt-8 mb-4 text-6xl font-bold uppercase">
+              ओम टेंट हाउस
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Our incredible Next.js community got together in San Francisco for
-              our first ever in-person conference!
+              परवेज एण्ड चिंडालिया हॉस्पिटल के सामने, गणेश मंदिर रोड, न्यू घड़साना
             </p>
-            <a
-              className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
-              href="https://vercel.com/new/clone?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-cloudinary&project-name=nextjs-image-gallery&repository-name=with-cloudinary&env=NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,CLOUDINARY_API_KEY,CLOUDINARY_API_SECRET,CLOUDINARY_FOLDER&envDescription=API%20Keys%20from%20Cloudinary%20needed%20to%20run%20this%20application"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Clone and Deploy
-            </a>
+            {/* <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
+              शानदार क्वालिटी के टेंट, बेहतरीन सर्विस, क्वालिटी केटरिंग, हलवाई, लाइट डेकोरेशन, नेपाली वेटर, लेटेस्ट डिजाइन के टेंट।
+            </p> */}
+            <p className="max-w-[40ch] text-white sm:max-w-[32ch]">
+              ओम राजोरिया - 94145 15321 <br /> कमल राजोरिया - 97725 37554 <br /> गिरधारी सोखल - 97859 84553
+            </p>
           </div>
           {images.map(({ id, public_id, format, blurDataUrl }) => (
             <Link
@@ -101,36 +96,6 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           ))}
         </div>
       </main>
-      <footer className="p-6 text-center text-white/80 sm:p-12">
-        Thank you to{' '}
-        <a
-          href="https://edelsonphotography.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Josh Edelson
-        </a>
-        ,{' '}
-        <a
-          href="https://www.newrevmedia.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Jenny Morgan
-        </a>
-        , and{' '}
-        <a
-          href="https://www.garysextonphotography.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Gary Sexton
-        </a>{' '}
-        for the pictures.
-      </footer>
     </>
   )
 }
